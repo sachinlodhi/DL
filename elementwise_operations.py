a009782935cfff932127fe6378a74d68cd58fe75
Sachin Lodhi <sachinlodhi8614@gmail.com>
2022-06-28 19:15:41 +0530

    Update elementwise_operations.py

--- elementwise_operations.py
+++ elementwise_operations.py
@@ -23,6 +23,14 @@ def vector_avg(vec):
         sum+=i
     return sum/len(vec_a)
 
+
+def dot_product(vec_a, vec_b):
+    print(vec_a, vec_b)
+    ans = 0
+    for i in range(len(vec_a)):
+        ans+=vec_a[i]*vec_b[i]
+    return ans
+
 inputs = [8.3,9.4, 2.4, 9.2, 9, 12]
 weights = [0.2, 0.5, 0.1, 0.56, 0.63]
 
